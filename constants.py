e5cdb477b933b03585082007968b5b0cf4527b82
will chau <willchau@gmail.com>
2021-08-25 16:03:39 +0800

    added constants.py, helpers.py

---
+++ constants.py
@@ -0,0 +1,3 @@
+IMAGE = 'images/test.png'
+WINDOWSIZE = '900x900'
+TITLE = 'My game'
--- sevens.py
+++ sevens.py
@@ -1,10 +1,31 @@
 '''
-this is the (unoffical, incomplete) pseudocode for the sevens game 
+this is the offical, complete pseudocode for the sevens game 
 
-if NUMBER % 7 == 0 or NUMBER contains '7'
-    stay silent
-else
-    say the number
+loop for NUMBER = 1 to 100
+    if NUMBER mod 7 = 0 OR NUMBER contains '7'
+        stay silent
+    else
+        say the number
+end loop
 
 '''
 
+
+
+
+# this is a comment
+'''
+this is a comment block
+
+'''
+
+number = str(5)
+
+
+
+
+for number in range(1, 100):
+    if number % 7 == 0 or :
+        print('shhhhhhhh')
+    else:
+        print(number)
