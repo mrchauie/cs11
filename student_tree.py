4d3003bddf929cc2321d464791c736cee18b318a
will chau <willchau@gmail.com>
2021-09-09 15:10:04 +0800

    added binary tree

---
+++ student_tree.py
@@ -0,0 +1,75 @@
+class Node:
+    """An Node of the Binary Tree.
+    """
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def insert(self, data):
+    # Compare the new value with the parent node
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+    # Print the tree
+    def printTree(self):
+        if self.left:
+            self.left.PrintTree()
+        print(self.data),
+        if self.right:
+            self.right.PrintTree()
+
+    # Inorder traversal
+    # Left -> Root -> Right
+    def inorderTraversal(self, root):
+      res = []
+      if root:
+         res = self.inorderTraversal(root.left)
+         res.append(root.data)
+         res = res + self.inorderTraversal(root.right)
+      return res
+
+    # Preorder traversal
+    # Root -> Left ->Right
+    def preorderTraversal(self, root):
+        res = []
+        if root:
+            res.append(root.data)
+            res = res + self.preorderTraversal(root.left)
+            res = res + self.preorderTraversal(root.right)
+        return res
+
+    # Postorder traversal
+    # Left ->Right -> Root
+    def postorderTraversal(self, root):
+        res = []
+        if root:
+            res = self.postorderTraversal(root.left)
+            res = res + self.postorderTraversal(root.right)
+            res.append(root.data)
+        return res
+
+
+#test
+
+root = Node(27)
+root.insert(14)
+root.insert(35)
+root.insert(10)
+root.insert(19)
+root.insert(31)
+root.insert(42)
+print(root.inorderTraversal(root))
+print(root.preorderTraversal(root))
+print(root.postorderTraversal(root))
