2380d80e0811e0d27160d7a413cb6a7b6280b14c
will chau <willchau@gmail.com>
2021-10-27 12:39:21 +0800

    added comments to assessment 31

--- assessment31.py
+++ assessment31.py
@@ -2,12 +2,18 @@ import random
 import requests
 
 def generate_grades():
+    '''
+    Generates an array of 2000 student numbers (ordered starting at 10001) and a random grade for the student
+    '''
     grades = []
     for i in range(10001, 12000):
         grades.append([i, random.randint(1,100)])
     return grades
 
 def generate_personal():
+    '''
+    Generates an array of 2000 student numbers (ordered starting at 10001) and a random name for the student
+    '''
     api_url = "https://api.namefake.com/english-canada/random/"
     personal = []
     for i in range(10001, 10003):
